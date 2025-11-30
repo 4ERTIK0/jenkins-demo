e8a14a56c533ad644abe8b58a2949da74c9c639a
4ERTIK0 <144389926+4ERTIK0@users.noreply.github.com>
2025-11-30 18:53:07 +0300

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "===================================="
+echo "Hello from Jenkins Pipeline!"
+echo "This is a demo script"
+echo "Current date: $(date)"
+echo "===================================="
